53d9f954297f02a81328cfa6701966f0b6476a75
Darius Cepulis <de.cepulis@gmail.com>
2025-05-17 16:14:35 -0500

    Point to TestFlight

--- dainorelisapp.com/src/utils/urls.ts
+++ dainorelisapp.com/src/utils/urls.ts
@@ -1,3 +1,4 @@
-export const appStoreUrl = 'https://apps.apple.com/app/id1116436510';
+// export const appStoreUrl = 'https://apps.apple.com/app/id1116436510';
+export const appStoreUrl = 'https://testflight.apple.com/join/8SxfABwA';
 // export const playStore = 'https://play.google.com/store/apps/details?id=com.dainorelis.dainorelis'
 export const playStoreUrl = 'https://forms.gle/4yLs4jV7zAyWxx5Q9';
