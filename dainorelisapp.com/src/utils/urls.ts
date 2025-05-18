013bad90ecb670f816db73ae53168bb96c39a6cd
Darius Cepulis <de.cepulis@gmail.com>
2025-05-18 07:59:10 -0500

    Update urls.ts

--- dainorelisapp.com/src/utils/urls.ts
+++ dainorelisapp.com/src/utils/urls.ts
@@ -1,4 +1,3 @@
-// export const appStoreUrl = 'https://apps.apple.com/app/id1116436510';
-export const appStoreUrl = 'https://testflight.apple.com/join/8SxfABwA';
-// export const playStore = 'https://play.google.com/store/apps/details?id=com.dainorelis.dainorelis'
+export const appStoreUrl = 'https://apps.apple.com/app/id1116436510';
+// export const playStoreUrl = 'https://play.google.com/store/apps/details?id=com.dainorelis.dainorelis'
 export const playStoreUrl = 'https://forms.gle/4yLs4jV7zAyWxx5Q9';
