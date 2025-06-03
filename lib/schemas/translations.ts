8b7e454f5f081faab89bd0dd4ba29ff1d7206566
Darius Cepulis <dcepulis@mux.com>
2025-06-03 08:30:58 -0700

    Update translations.ts

--- lib/schemas/translations.ts
+++ lib/schemas/translations.ts
@@ -3,6 +3,7 @@ import { z } from 'zod';
 export const TranslationsSchema = z.object({
   Title: z.string(),
   'Variant Name': z.string(),
+  'EN Variant Name': z.string(),
   Lyrics: z.string(),
   'AI Generated': z.boolean().optional(),
   Notes: z.string().optional(),
