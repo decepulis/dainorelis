5ef258843d5d6c71d1934f4183a0fe58d314be2a
Darius Cepulis <dcepulis@mux.com>
2025-06-04 23:31:23 -0700

    Simplifying SystemView

--- lib/components/SystemView.tsx
+++ lib/components/SystemView.tsx
@@ -1,5 +1,5 @@
 import { forwardRef } from 'react';
-import { Platform, StyleProp, View, ViewStyle, useColorScheme } from 'react-native';
+import { Platform, StyleProp, View, ViewStyle } from 'react-native';
 
 import { BlurView } from 'expo-blur';
 
@@ -8,56 +8,51 @@ import { useThemeColor } from '../hooks/useThemeColor';
 type Props = {
   children?: React.ReactNode;
   style?: StyleProp<ViewStyle>;
-  variant: 'primary' | 'background';
   shadow?: boolean;
 } & BlurView['props'];
 
 /**
  * A view that gets blurry on iOS but stays solid on Android
  */
-const SystemView = forwardRef<View, Props>(
-  ({ children, style, variant = 'background', shadow = true, ...rest }, ref) => {
-    const isDark = useColorScheme() === 'dark';
-    const primary = useThemeColor('primary');
-    const background = useThemeColor('background');
+const SystemView = forwardRef<View, Props>(({ children, style, shadow = true, ...rest }, ref) => {
+  const primary = useThemeColor('primary');
 
-    if (Platform.OS === 'ios') {
-      return (
-        <BlurView
-          ref={ref as any}
-          // no need to listen to reduce transparency on iOS; iOS does this for us
-          intensity={variant === 'primary' ? 65 : 85}
-          tint={variant === 'primary' ? 'systemChromeMaterialDark' : isDark ? 'dark' : 'extraLight'}
-          style={[
-            {
-              backgroundColor: variant === 'primary' ? `${primary}88` : `${background}66`,
-            },
-            style,
-          ]}
-          {...rest}
-        >
-          {children}
-        </BlurView>
-      );
-    }
+  if (Platform.OS === 'ios') {
     return (
-      <View
-        ref={ref}
+      <BlurView
+        ref={ref as any}
+        // no need to listen to reduce transparency on iOS; iOS does this for us
+        intensity={55}
+        tint={'systemChromeMaterialDark'}
         style={[
           {
-            backgroundColor: variant === 'primary' ? primary : background,
-            // TODO header overflow hidden is preventing this from drawing
-            boxShadow: shadow ? '0px 0px 10px rgba(64, 64, 64, 0.1)' : undefined,
+            backgroundColor: `${primary}88`,
           },
           style,
         ]}
         {...rest}
       >
         {children}
-      </View>
+      </BlurView>
     );
   }
-);
+  return (
+    <View
+      ref={ref}
+      style={[
+        {
+          backgroundColor: primary,
+          // TODO header overflow hidden is preventing this from drawing
+          boxShadow: shadow ? '0px 0px 10px rgba(64, 64, 64, 0.1)' : undefined,
+        },
+        style,
+      ]}
+      {...rest}
+    >
+      {children}
+    </View>
+  );
+});
 
 // Add display name for better debugging
 SystemView.displayName = 'SystemView';
